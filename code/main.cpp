b24aa5da9e1bb362c53a0b58d0e554e0fbc8d60e
José Eduardo <103583698+Wally1906@users.noreply.github.com>
2024-03-06 23:13:43 +0000

    Pontos feitos maybe, print ainda esta morto

--- code/main.cpp
+++ code/main.cpp
@@ -145,75 +145,72 @@ void cone(float radius, float height, int slices,int stacks) {
 	glEnd();
 }
 
-void sphere(float radius, int slices, int stacks) {
+void sphere(double radius, int slices, int stacks) {
 
 	int i;
 	float step;
 	float n_stacks;
-	tuple<float, float, float>** matrix = new tuple<float, float, float>* [slices];
-	for (int i = 0; i < slices; ++i)
-		matrix[i] = new tuple<float, float, float>[stacks];
-
-
+	//tuple<float, float, float>** matrix = new tuple<float, float, float>* [stacks];
+	//for (int i = 0; i < stacks; ++i)
+		//matrix[i] = new tuple<float, float, float>[slices];
+		
+	
 	step = 360.0 / slices; 
-	n_stacks = radius / stacks; 
-
-	glBegin(GL_TRIANGLES);
-
+	n_stacks = 180 / stacks;
+	vector<tuple<float, float, float>> pontos;
+ 
 	// body
-	for (i = 0; i <= slices; i++) {
-
-		/*for (int j = 1; j < stacks; j++) {
-
-			//Triângulo da metade da esquerda
-			glVertex3f(cos(i * step * M_PI / 180.0) * ((radius / stacks) * j), radius - n_stacks * j, -sin(i * step * M_PI / 180.0) * ((radius / stacks) * j));
-			glVertex3f(cos(i * step * M_PI / 180.0) * ((radius / stacks) * (j + 1)), radius - n_stacks * (j + 1), -sin(i * step * M_PI / 180.0) * ((radius / stacks) * (j + 1)));
-			glVertex3f(cos((i + 1) * step * M_PI / 180.0) * ((radius / stacks) * (j + 1)), radius - n_stacks * (j + 1), -sin((i + 1) * step * M_PI / 180.0) * ((radius / stacks) * (j + 1)));
-
-			//Triângulo da metade da direita
-			glVertex3f(cos((i + 1) * step * M_PI / 180.0) * ((radius / stacks) * j), radius - n_stacks * j, -sin((i + 1) * step * M_PI / 180.0) * ((radius / stacks) * j));
-			glVertex3f(cos(i * step * M_PI / 180.0) * ((radius / stacks) * j), radius - n_stacks * j, -sin(i * step * M_PI / 180.0) * ((radius / stacks) * j));
-			glVertex3f(cos((i + 1) * step * M_PI / 180.0) * ((radius / stacks) * (j + 1)), radius - n_stacks * (j + 1), -sin((i + 1) * step * M_PI / 180.0) * ((radius / stacks) * (j + 1)));
-		}*/
-	}
 	for (i = 0; i < stacks; i++) {
-		float lat = 180/stacks * i;
+		float lat = n_stacks*i;
 		for (int j = 0; j < slices; j++) {
-			float lon = j * step;
-			float x = radius * sin(lat) * cos(lon);
-			float z = radius * sin(lat) * sin(lon);
-			float y = radius * cos(lat);
-			matrix[i][j] = make_tuple(x, y, z);
+			double lon = j * step;
+
+			const double currentStack = i * lat;
+			const double currentSlice = j * lon;
+			const double nextStack = currentStack + lat;
+			const double nextSlice = currentSlice + lon;
+
+			tuple<double, double, double> p0 = { (radius * sin(nextStack) * sin(nextSlice)), (radius * cos(nextStack)), (radius * sin(nextStack) * cos(nextSlice))};
+			tuple<double, double, double> p1 = { (radius * sin(nextStack) * sin(currentSlice)), (radius * cos(nextStack)), (radius * sin(nextStack) * cos(currentSlice)) };
+			tuple<double, double, double> p2 = { (radius * sin(currentStack) * sin(nextSlice)), (radius * cos(currentStack)), (radius * sin(currentStack) * cos(nextSlice)) };
+			tuple<double, double, double> p3 = { (radius * sin(currentStack) * sin(currentSlice)), (radius * cos(currentStack)), (radius * sin(currentStack) * cos(currentSlice)) };
+
+			pontos.push_back(p0);
+			pontos.push_back(p1);
+			pontos.push_back(p3);
+			pontos.push_back(p2);
+			pontos.push_back(p3);
+			pontos.push_back(p0);
+			//matrix[i][j] = make_tuple(x, y, z);
 		}
 
 	}
+
+
 	for (i = 0; i < stacks-1; i++) {
 		glBegin(GL_TRIANGLES);
 		for (int j = 1; j < slices-1; j++) {
 
 			//Triângulo da metade da esquerda
-			glVertex3f(get<0>(matrix[i][j]), get<1>(matrix[i][j]), get<2>(matrix[i][j]));
-			//glVertex3f(get<0>(matrix[i][j+1]), get<1>(matrix[i][j+1]), get<2>(matrix[i][j+1]));
-			//glVertex3f(get<0>(matrix[i+1][j]), get<1>(matrix[i+1][j]), get<2>(matrix[i+1][j+1]));
-
-			//glVertex3f(cos(i * step * M_PI / 180.0) * ((radius / stacks) * (j + 1)), radius - n_stacks * (j + 1), -sin(i * step * M_PI / 180.0) * ((radius / stacks) * (j + 1)));
-			//glVertex3f(cos((i + 1) * step * M_PI / 180.0) * ((radius / stacks) * (j + 1)), radius - n_stacks * (j + 1), -sin((i + 1) * step * M_PI / 180.0) * ((radius / stacks) * (j + 1)));
-
+			//glVertex3f(get<0>(matrix[i+1][j+1]), get<1>(matrix[i+1][j+1]), get<2>(matrix[i+1][j+1]));
 			//Triângulo da metade da direita
-			glVertex3f(get<0>(matrix[i+1][j]), get<1>(matrix[i+1][j]), get<2>(matrix[i+1][j]));
-			glVertex3f(get<0>(matrix[i][j+1]), get<1>(matrix[i][j+1]), get<2>(matrix[i][j+1]));
 			//glVertex3f(get<0>(matrix[i][j]), get<1>(matrix[i][j]), get<2>(matrix[i][j]));
-			//glVertex3f(get<0>(matrix[i+1][j]), get<1>(matrix[i][j+1]), get<2>(matrix[i+1][j+1]));
-			
-			//glVertex3f(cos((i + 1) * step * M_PI / 180.0) * ((radius / stacks) * j), radius - n_stacks * j, -sin((i + 1) * step * M_PI / 180.0) * ((radius / stacks) * j));
-			//glVertex3f(cos(i * step * M_PI / 180.0) * ((radius / stacks) * j), radius - n_stacks * j, -sin(i * step * M_PI / 180.0) * ((radius / stacks) * j));
-			//glVertex3f(cos((i + 1) * step * M_PI / 180.0) * ((radius / stacks) * (j + 1)), radius - n_stacks * (j + 1), -sin((i + 1) * step * M_PI / 180.0) * ((radius / stacks) * (j + 1)));
-		}
+			//glVertex3f(get<0>(matrix[i+1][j]), get<1>(matrix[i+1][j]), get<2>(matrix[i+1][j]));
+			//glVertex3f(get<0>(matrix[i][j+1]), get<1>(matrix[i][j+1]), get<2>(matrix[i][j+1]));
+			//glVertex3f(get<0>(matrix[i][j]), get<1>(matrix[i][j]), get<2>(matrix[i][j]));
+			//glVertex3f(get<0>(matrix[i + 1][j + 1]), get<1>(matrix[i + 1][j + 1]), get<2>(matrix[i + 1][j + 1]));
+			}
 		glEnd();
 	}
-	for (int i = 0; i < slices; ++i)
-		delete[] matrix[i];
-	delete[] matrix;
+
+	glBegin(GL_TRIANGLES);
+	for (auto& element : pontos) {
+		glVertex3f(get<0>(element), get<1>(element), get<2>(element));
+	}
+	glEnd();
+	//for (int i = 0; i < slices; ++i)
+		//delete[] matrix[i];
+	//delete[] matrix;
 }
 
 void plane(int length, int divisions){
@@ -410,7 +407,8 @@ void renderScene(void) {
 	//cylinder0(1,2,10);
 	//plane(2,7);
 	//box(2,5);
-	sphere(1,20,20);
+	//cone(1, 2, 20, 20);
+	sphere(1,30,30);
 
 	// End of frame
 	glutSwapBuffers();
